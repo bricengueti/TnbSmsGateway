e8563c345124220b72855bc2cd70793b0442adea
bricengueti <tchongouebricengueti@gmail.com>
2026-07-18 18:31:25 +0200

    cicd in develop

--- src/main/java/TNB/SmsGateway/config/SecurityConfig.java
+++ src/main/java/TNB/SmsGateway/config/SecurityConfig.java
@@ -43,7 +43,6 @@ public class SecurityConfig {
                                 "/webjars/**",
                                 "/actuator/health"
                         ).permitAll()
-
                         // ===== JWT - Dashboard =====
                         .requestMatchers(
                                 "/v1/webhook/**",
